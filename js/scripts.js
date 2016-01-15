acde44fd9b5c664edd5de6fc94622546ced0790b
eddie <e.jamesduro@gmail.com>
2016-01-15 09:26:24 -0800

    pull destinations from array

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
 	var total = 0;
 	var userInput;
 	var destinationArr = ['Seoul', 'Tokyo', 'Berlin', 'Berlin', 'London', 'London', 'Dublin', 'Hanoi', 'Hanoi', 'Bangkok', 'Bangkok', 'Shanghai', 'Shanghai','Paris', 'Paris', 'Moscow'];
+
 	$("form").submit(function(event) {
-		for(var i = 1; i < 6; i++) {
+		for(var i = 1; i < 6; i ++) {
 			var userInput = $("input[name=radios" + i + "]:checked").val();
-			console.log(userInput)
+			userInput = parseInt(userInput);
+			total += userInput;
 		}
+		var destinations = (destinationArr[total]);
+		console.log(destinations);
+		event.preventDefault();
+	});
+	$('.refresh-btn').click(function() {
+		window.location.reload();
 	});
 });
