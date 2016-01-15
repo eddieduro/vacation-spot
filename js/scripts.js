195a0717cfc2d646c67790017d15055f6989491b
eddie <e.jamesduro@gmail.com>
2016-01-15 10:12:42 -0800

    pictures show up when user hits submit button

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var total = 0;
 	var userInput;
 	var destinationArr = ['Seoul', 'Tokyo', 'Berlin', 'Berlin', 'London', 'London', 'Dublin', 'Hanoi', 'Hanoi', 'Bangkok', 'Bangkok', 'Shanghai', 'Shanghai','Paris', 'Paris', 'Moscow'];
+	var destinationPic = ['seoul', 'tokyo', 'berlin', 'berlin', 'london', 'london', 'dublin', 'hanoi', 'hanoi', 'bangkok', 'bangkok', 'shanghai', 'shanghai','paris', 'paris', 'moscow'];
 
 	$("form").submit(function(event) {
 		for(var i = 1; i < 6; i ++) {
@@ -10,7 +11,9 @@ $(document).ready(function() {
 			total += userInput;
 		}
 		var destinations = (destinationArr[total]);
+		var pictures = (destinationPic[total]);
 		$("h1").text(destinations).prepend("<h4>Your next destination is :</h4>");
+		$("div.hero").addClass(pictures);
 		event.preventDefault();
 	});
 	$('.refresh-btn').click(function() {
