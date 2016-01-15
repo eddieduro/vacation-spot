1c1ccc6016a822dbdd92fcd05eaa9f2f0fd5382f
eddie <e.jamesduro@gmail.com>
2016-01-15 09:00:17 -0800

    collects val from form input

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
 	var total = 0;
 	var userInput;
 	var destinationArr = ['Seoul', 'Tokyo', 'Berlin', 'Berlin', 'London', 'London', 'Dublin', 'Hanoi', 'Hanoi', 'Bangkok', 'Bangkok', 'Shanghai', 'Shanghai','Paris', 'Paris', 'Moscow'];
-
+	$("form").submit(function(event) {
+		for(var i = 1; i < 6; i++) {
+			var userInput = $("input[name=radios" + i + "]:checked").val();
+			console.log(userInput)
+		}
+	});
 });
