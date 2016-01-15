12c390346fde1785b15a9cf48dabf558beb25650
eddie <e.jamesduro@gmail.com>
2016-01-15 08:55:27 -0800

    define variables

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	var total = 0;
+	var userInput;
+	var destinationArr = ['Seoul', 'Tokyo', 'Berlin', 'Berlin', 'London', 'London', 'Dublin', 'Hanoi', 'Hanoi', 'Bangkok', 'Bangkok', 'Shanghai', 'Shanghai','Paris', 'Paris', 'Moscow'];
+
+});
