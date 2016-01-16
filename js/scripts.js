2fa6d8af876e5f07f873d9eeb195e9382609d6ae
eddieduro <student@epicodus.com>
2016-01-15 16:37:18 -0800

    add links array for each destionation

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	//store picture css class names in array
 	var destinationPic = ['seoul', 'tokyo',  'shanghai', 'shanghai', 'hanoi', 'hanoi', 'bangkok', 'bangkok', 'berlin', 'berlin', 'london', 'london', 'dublin', 'paris', 'paris', 'chernobyl'];
 
-
+	var destinationLink = ['http://www.visitseoul.net/en/index.do', 'https://www.gotokyo.org/en/', 'http://www.travelandleisure.com/travel-guide/shanghai','http://www.travelandleisure.com/travel-guide/shanghai','http://www.lonelyplanet.com/vietnam/hanoi', 'http://www.lonelyplanet.com/vietnam/hanoi', 'http://www.tourismthailand.org/About-Thailand/Destination/Bangkok', 'http://www.tourismthailand.org/About-Thailand/Destination/Bangkok', 'http://www.visitberlin.de/en', 'http://www.visitberlin.de/en', 'http://www.visitlondon.com/', 'http://www.visitlondon.com/', 'http://www.visitdublin.com/home/', 'http://en.parisinfo.com/', 'http://en.parisinfo.com/', 'http://www.tourkiev.com/'];
 	//Section that reveals your destination
 	$("form").submit(function(event) {
 		for(var i = 1; i < 6; i ++) {
