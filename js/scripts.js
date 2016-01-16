03a68adeaf73b8f66ecd94f35a3b2caffa851dd1
eddieduro <student@epicodus.com>
2016-01-15 16:35:59 -0800

    add links array for each destionation

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	//store picture css class names in array
 	var destinationPic = ['seoul', 'tokyo',  'shanghai', 'shanghai', 'hanoi', 'hanoi', 'bangkok', 'bangkok', 'berlin', 'berlin', 'london', 'london', 'dublin', 'paris', 'paris', 'chernobyl'];
 
+	var destinationLink = ['http://www.visitseoul.net/en/index.do', 'https://www.gotokyo.org/en/', 'http://www.travelandleisure.com/travel-guide/shanghai','http://www.travelandleisure.com/travel-guide/shanghai','http://www.lonelyplanet.com/vietnam/hanoi', 'http://www.lonelyplanet.com/vietnam/hanoi', 'http://www.tourismthailand.org/About-Thailand/Destination/Bangkok', 'http://www.tourismthailand.org/About-Thailand/Destination/Bangkok', 'http://www.visitberlin.de/en', 'http://www.visitberlin.de/en', 'http://www.visitlondon.com/', 'http://www.visitlondon.com/', 'http://www.visitdublin.com/home/', 'http://en.parisinfo.com/', 'http://en.parisinfo.com/', 'http://www.tourkiev.com/'];
 
 	//Section that reveals your destination
 	$("form").submit(function(event) {
@@ -19,6 +20,7 @@ $(document).ready(function() {
 		//set variables equal to array with index of [total] from loop above
 		var destinations = (destinationArr[total]);
 		var pictures = (destinationPic[total]);
+		var links = (destinationLink[total]);
 
 		if ($("h1").text(destinations).prepend("<h4>Your next destination is :</h4>")) {
 			$("div.hero").addClass(pictures);
